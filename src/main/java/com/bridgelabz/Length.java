dba4677df7932e2d915fa950c307d1054ab8f331
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-10-29 16:51:38 +0530

    Added: when two inputs are 0feet and 0feet returnEquals

--- src/main/java/com/bridgelabz/Length.java
+++ src/main/java/com/bridgelabz/Length.java
@@ -1,6 +1,19 @@
 package com.bridgelabz;
 
+import java.util.Objects;
+
 public class Length {
+    private final double value;
+
     public Length(double value) {
+        this.value=value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length = (Length) o;
+        return Double.compare(length.value, value) == 0;
     }
 }
