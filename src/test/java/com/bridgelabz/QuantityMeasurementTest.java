9de344a1fd3ab83cae15061850ca0c5cf185823c
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-10-31 14:26:45 +0530

    Refactored:The Code has changed to meet the DRY principle

--- src/test/java/com/bridgelabz/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementTest.java
@@ -6,149 +6,142 @@ import org.junit.jupiter.api.Test;
 public class QuantityMeasurementTest {
     @Test
     void given0FeetAnd0Feet_ShouldReturnEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.0);
+        QmLengths feet1 = new QmLengths(QmLengths.Unit.FEET, 0.0);
+        QmLengths feet2 = new QmLengths(QmLengths.Unit.FEET, 0.0);
         Assertions.assertEquals(feet1, feet2);
     }
 
     @Test
     void given0FeetAnd1Feet_ShouldReturnNotEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        QmLengths feet1 = new QmLengths(QmLengths.Unit.FEET, 0.0);
+        QmLengths feet2 = new QmLengths(QmLengths.Unit.FEET, 1.0);
         Assertions.assertNotEquals(feet1, feet2);
     }
 
     @Test
     void given0FeetAndNull_ShouldReturnNotEquals() {
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = null;
+        QmLengths feet1 = new QmLengths(QmLengths.Unit.FEET, 1.0);
+        QmLengths feet2 = null;
         Assertions.assertNotEquals(feet1, feet2);
     }
 
     @Test
     void given0FeetAnd0FeetFromDiffReference_ShouldReturnFalse() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.0);
+        QmLengths feet1 = new QmLengths(QmLengths.Unit.FEET, 0.0);
+        QmLengths feet2 = new QmLengths(QmLengths.Unit.FEET, 0.0);
         boolean result = feet1 == feet2;
         Assertions.assertFalse(result);
     }
 
     @Test
     void given0FeetFromSameReference_ShouldReturnTrue() {
-        Feet feet = new Feet(0.0);
+        QmLengths feet = new QmLengths(QmLengths.Unit.FEET, 0.0);
         boolean result = feet == feet;
         Assertions.assertTrue(result);
     }
 
     @Test
     void givenFeetWithDiffType_shouldReturnNotEqual() {
-        Feet feet1 = new Feet(0.0);
+        QmLengths feet1 = new QmLengths(QmLengths.Unit.FEET, 0.0);
         boolean feet2 = true;
         Assertions.assertNotEquals(feet1, feet2);
     }
 
     @Test
     void given0InchAnd0Inch_ShouldReturnEqual() {
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = new Inch(0.0);
+        QmLengths inch1 = new QmLengths(QmLengths.Unit.INCH, 0.0);
+        QmLengths inch2 = new QmLengths(QmLengths.Unit.INCH, 0.0);
         Assertions.assertEquals(inch1, inch2);
     }
 
     @Test
     void given0InchAndNull_ShouldReturnNotEqual() {
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = null;
+        QmLengths inch1 = new QmLengths(QmLengths.Unit.INCH, 0.0);
+        QmLengths inch2 = null;
         Assertions.assertNotEquals(inch1, inch2);
     }
 
     @Test
     void givenInchWithSameReference_ShouldReturnTrue() {
-        Inch inch = new Inch(1.0);
+        QmLengths inch = new QmLengths(QmLengths.Unit.INCH, 1.0);
         boolean result = inch == inch;
         Assertions.assertTrue(result);
     }
 
     @Test
     void givenInchWithDiffReference_ShouldReturnFalse() {
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = new Inch(0.0);
+        QmLengths inch1 = new QmLengths(QmLengths.Unit.INCH, 0.0);
+        QmLengths inch2 = new QmLengths(QmLengths.Unit.INCH, 0.0);
         boolean result = inch1 == inch2;
         Assertions.assertFalse(result);
     }
 
     @Test
-    void givenInchandDifferentType_ShouldReturnNotEqual() {
-        Inch inch = new Inch(0.0);
+    void givenInchAndDifferentType_ShouldReturnNotEqual() {
+        QmLengths inch = new QmLengths(QmLengths.Unit.INCH, 0.0);
         boolean inch1 = true;
         Assertions.assertNotEquals(inch, inch1);
     }
 
     @Test
     void given1InchAnd1Inch_ShouldReturnEqual() {
-        Inch inch1 = new Inch(2.0);
-        Inch inch2 = new Inch(2.0);
+        QmLengths inch1 = new QmLengths(QmLengths.Unit.INCH, 2.0);
+        QmLengths inch2 = new QmLengths(QmLengths.Unit.INCH, 2.0);
         Assertions.assertEquals(inch1, inch2);
     }
 
     @Test
     void given1Feet_comparedTo12Inches_ShouldReturnEquals() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        Feet feet = new Feet(1.0);
-        Inch result = quantityMeasurement.convertFtToInch(feet.getValue());
-        Assertions.assertEquals(12.0, result.getValue());
+        QmLengths feet = new QmLengths(QmLengths.Unit.FEET, 1.0);
+        double result = feet.convertFtToInch();
+        Assertions.assertEquals(12.0, result);
     }
 
     @Test
     void given0YardAnd0Yard_ShouldReturnEqual() {
-        Yard yard1 = new Yard(0.0);
-        Yard yard2 = new Yard(0.0);
+        QmLengths yard1 = new QmLengths(QmLengths.Unit.YARD, 0.0);
+        QmLengths yard2 = new QmLengths(QmLengths.Unit.YARD, 0.0);
     }
 
     @Test
     void given3FeetAnd1Yard_WhenCompared_ShouldReturnEqual() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        Feet feet = new Feet(3.0);
-        Yard result = quantityMeasurement.convertFtToYard(feet.getValue());
-        Assertions.assertEquals(1.0, result.getValue(), 0.1);
+        QmLengths feet = new QmLengths(QmLengths.Unit.FEET, 3.0);
+        double result = feet.convertFtToYard();
+        Assertions.assertEquals(1.0, result);
     }
 
     @Test
     void given1FeetAnd1Yard_WhenCompared_ShouldReturnNotEquals() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        Feet feet = new Feet(1.0);
-        Yard result = quantityMeasurement.convertFtToYard(feet.getValue());
-        Assertions.assertNotEquals(1.0, result.getValue(), 0.1);
+        QmLengths feet = new QmLengths(QmLengths.Unit.FEET, 1.0);
+        double result = feet.convertFtToYard();
+        Assertions.assertNotEquals(1.0, result);
     }
 
     @Test
     void given1InchAnd1Yard_WhenCompared_ShouldReturnNotEquals() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        Inch inch = new Inch(1.0);
-        Yard result = quantityMeasurement.convertInchToYard(inch.getValue());
-        Assertions.assertNotEquals(1.0, result.getValue(), 0.1);
+        QmLengths inch = new QmLengths(QmLengths.Unit.INCH, 1.0);
+        double result = inch.convertInchToYard();
+        Assertions.assertNotEquals(1.0, result);
     }
 
     @Test
     void given36nchAnd1Yard_WhenCompared_ShouldReturnEquals() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        Inch inch = new Inch(36.0);
-        Yard result = quantityMeasurement.convertInchToYard(inch.getValue());
-        Assertions.assertEquals(1.0, result.getValue(), 0.1);
+        QmLengths inch = new QmLengths(QmLengths.Unit.INCH, 36.0);
+        double result = inch.convertInchToYard();
+        Assertions.assertEquals(1.0, result);
     }
 
     @Test
     void given1YardAnd36Inch_WhenCompared_ShouldReturnEquals() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        Yard yard = new Yard(1.0);
-        Inch result = quantityMeasurement.convertYardToInch(yard.getValue());
-        Assertions.assertEquals(36.0, result.getValue(), 0.1);
+        QmLengths yard = new QmLengths(QmLengths.Unit.YARD, 1.0);
+        double result = yard.convertYardToInch();
+        Assertions.assertEquals(36.0, result);
     }
 
     @Test
     void given1YardAnd3Feet_WhenCompared_ShouldReturnEquals() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        Yard yard = new Yard(1.0);
-        Feet result = quantityMeasurement.convertYardToFeet(yard.getValue());
-        Assertions.assertEquals(3.0, result.getValue(), 0.1);
+        QmLengths yard = new QmLengths(QmLengths.Unit.YARD, 1.0);
+        double result = yard.convertYardToFeet();
+        Assertions.assertEquals(3.0, result);
     }
 }
